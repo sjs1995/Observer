d7cdf0eaee7fbbbec4e2d850b5b88413b73fd0af
sjs1995 <13001080911@163.com>
2019-04-12 20:19:03 +0800

    first commit

---
+++ observer.js
@@ -0,0 +1,42 @@
+var EventCenter = function(){
+    var events = {};
+    /*
+    {
+      my_event: [{handler: function(data){xxx}}, {handler: function(data){yyy}}]
+    }
+    */
+    // 绑定事件 添加回调
+    function on(evt, handler){
+        events[evt] = events[evt] || [];
+        events[evt].push({
+            handlers:handler
+        })
+    }
+    function fire(evt, arg){
+        if(!events[evt]){
+            return 
+        }
+        for(var i=0; i < events[evt].length; i++){
+            events[evt][i].handlers(arg);
+        }
+    }
+    function off(evt){
+        delete events[evt];
+    }
+    return {
+        on:on,
+        fire:fire,
+        off:off
+    }
+}();
+
+var number = 2;
+EventCenter.on('click', function(data){
+    console.log('click 事件' + data + number++ +'次');
+});
+EventCenter.off('click');   //  只绑定一次
+EventCenter.on('click', function(data){
+    console.log('click 事件' + data + number++ +'次');
+});
+
+EventCenter.fire('click', '绑定');
